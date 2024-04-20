245beaf5792b29a6aa2124b45dea5ce3db1ea275
MangelC25 <150280388+MangelC25@users.noreply.github.com>
2024-04-20 11:48:18 -0500

    Commit 8.2

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,13 @@
 import { navbar } from "./Header/navbar.js";
+import { calculadora } from "./Calculadora/Calcualdora.js";
+import { History } from "./History/History.js";
+import { Memory } from "./Memoria/Memoria.js";
 import NavResposive from "./Header/navbatToggle.js";
 
-
 document.addEventListener("DOMContentLoaded", function () {
-    NavResposive();
     navbar();
+    calculadora();
+    History();
+    Memory();
+    NavResposive();
 });
